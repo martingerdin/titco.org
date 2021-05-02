b232bbf5299b0448a599058b24a2017e66b91d13
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-05-03 01:16:55 +0200

    Sort publication list

--- pages/publications.tsx
+++ pages/publications.tsx
@@ -10,25 +10,31 @@ const GridStyled = styled.div`
 `;
 
 export default function PublicationsPage() {
+  const sortedPublications = [...publications]
+    .sort((a, b) => {
+      return b.YEAR - a.YEAR;
+    });
+  
   return (
     <Layout title="Publications">
 	<section className="section">
 	    <GridStyled>
-		{publications.map((publication) => {
-		  const {AUTHOR, TITLE, VOLUME, NUMBER, URL, JOURNAL, YEAR, MONTH, PAGES} = publication;
-		  return(
-		    <Publication
-		      authors={AUTHOR.join(", ")}
-		      title={TITLE}
-		      url={URL}
-		      journal={JOURNAL}
-		      year={YEAR.toString()}
-		      month={MONTH}
-		      volume={VOLUME}
-		      issue={NUMBER}
-		      pages={PAGES}
-		    />
-		  );
+		{sortedPublications
+		  .map((publication) => {
+		    const {AUTHOR, TITLE, VOLUME, NUMBER, URL, JOURNAL, YEAR, MONTH, PAGES} = publication;
+		    return(
+		      <Publication
+			authors={AUTHOR.join(", ")}
+			title={TITLE}
+			url={URL}
+			journal={JOURNAL}
+			year={YEAR.toString()}
+			month={MONTH}
+			volume={VOLUME}
+			issue={NUMBER}
+			pages={PAGES}
+		      />
+		    );
 		})}
 	    </GridStyled>
 	</section>
