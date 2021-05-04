c41dc23a8b24c7eb822ff325798cab7fc8c1b3fd
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-05-04 23:44:23 +0200

    Fix mismatch between server and client classNames for styled components
    So typescript complained about the .query method not being available
    on type {} and I guess that was because it was waiting for the
    getInitialProps to come back. Fixed it by switching to just plain
    javascript for now.

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import "../styles/styles.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+MyApp.getInitialProps = async function({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default MyApp;
