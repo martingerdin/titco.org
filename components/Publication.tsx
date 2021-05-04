55ab6f3fc6cfdba6b4cfc0ab96e8c6e62659a873
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-05-04 23:46:49 +0200

    Edit publication page to allow filtering of publications showing

--- components/Publication.tsx
+++ components/Publication.tsx
@@ -1,18 +1,23 @@
 import Icon from "@mdi/react";
 import { mdiOpenInNew } from "@mdi/js";
 import Link from "next/link";
+import styled from "styled-components";
 import capitalise from "../lib/capitalise";
 
+const CardStyled = styled.div`
+    max-width: 400px;
+`;
+
 interface publicationProps {
-  authors: string;
-  title: string;
-  url: string;
-  journal: string;
-  year: string;
-  month: string;
-  volume: string;
-  issue: string;
-  pages: string;
+  authors: string,
+  title: string,
+  url: string,
+  journal: string,
+  year: string,
+  month: string,
+  volume: string,
+  issue: string,
+  pages: string
 }
 
 export default function Publication({ authors, title, url, journal, year, month, volume, issue, pages }: publicationProps) {
@@ -24,30 +29,28 @@ export default function Publication({ authors, title, url, journal, year, month,
   
   return (
     <>
-	<div className="block">
-	    <div className="card">
-		<div className="card-content">
-		    <p className="title is-4 is-spaced">{title}</p>
-		    <p className="subtitle is-6">{authors}</p>
-		    <p className="content"><i>{journal}. {year} {Month};{volume}({issue}):{pages}.</i></p>
-		</div>
-		
-		{typeof url !== "undefined" &&
-		 <footer className="card-footer">
-
-		     <Link href={url} passHref>
-			 <a className="card-footer-item">
-			     <span>Fulltext</span>
-			     <span className="icon is-small mx-2">
-				 <Icon path={mdiOpenInNew} />
-			     </span>
-			 </a>
-		     </Link>
-
-		 </footer>
-		}
+	<CardStyled className="card">
+	    <div className="card-content">
+		<p className="title is-4 is-spaced">{title}</p>
+		<p className="subtitle is-6">{authors}</p>
+		<p className="content"><i>{journal}. {year} {Month};{volume}({issue}):{pages}.</i></p>
 	    </div>
-        </div>
+	    
+	    {typeof url !== "undefined" &&
+	     <footer className="card-footer">
+
+		 <Link href={url} passHref>
+		     <a className="card-footer-item">
+			 <span>Fulltext</span>
+			 <span className="icon is-small mx-2">
+			     <Icon path={mdiOpenInNew} />
+			 </span>
+		     </a>
+		 </Link>
+
+	     </footer>
+	    }
+        </CardStyled>
     </>
   )  
 }
--- pages/publications.tsx
+++ pages/publications.tsx
@@ -2,51 +2,62 @@ import styled from "styled-components";
 import publications from "../components/publications.json";
 import Publication from "../components/Publication";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
-const GridStyled = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 2rem;
-    
-    @media only screen and (max-width: 1000px) {
-    grid-template-columns: 1fr 1fr;
-    }
-    
-    @media only screen and (max-width: 800px) {
-    grid-template-columns: 1fr;
-    }
+const FlexStyled = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 `;
 
 export default function PublicationsPage() {
+  const [currentYear, setCurrentYear] = useState("all");
   const sortedPublications = [...publications]
     .sort((a, b) => {
       return b.YEAR - a.YEAR;
     });
-  
+  const years = [...new Set(sortedPublications.map((publication) => publication.YEAR))];
   return (
-    <Layout title="Publications" subtitle="Our published work 2012-2021">
+    <Layout title="Publications" subtitle={`Our published work ${years[years.length - 1]}-${new Date().getUTCFullYear()}`}>
+	<div className="buttons is-centered">
+	    {years.map((year, key) => {
+	      return <button
+		       key={key}
+		       className={`button ${currentYear === year.toString() ? "is-active" : ""}`}
+		       onClick={() => setCurrentYear(year.toString()) }>{year}</button>
+	    })}
+	</div>
 	<section className="section">
-	    <GridStyled>
-		{sortedPublications
-		  .map((publication, key) => {
-		    const {AUTHOR, TITLE, VOLUME, NUMBER, URL, JOURNAL, YEAR, MONTH, PAGES} = publication;
-		    return(
-		      <div className="block" key={key}>
-			  <Publication
-			    authors={AUTHOR.join(", ")}
-			    title={TITLE}
-			    url={URL}
-			    journal={JOURNAL}
-			    year={YEAR.toString()}
-			    month={MONTH}
-			    volume={VOLUME}
-			    issue={NUMBER}
-			    pages={PAGES}
-			  />
-		      </div>
-		    );
-		})}
-	    </GridStyled>
+	    <FlexStyled>
+	    {
+	      sortedPublications
+		.filter(publication => {
+		  if (currentYear === "all") {
+		    return true;
+		  } else {
+		    return publication.YEAR.toString() === currentYear;
+		  }
+		})
+		.map((publication, key) => {
+		  const {AUTHOR, TITLE, VOLUME, NUMBER, URL, JOURNAL, YEAR, MONTH, PAGES} = publication;
+		  return (
+		    <div className="mx-4 block" key={key}>
+			<Publication
+			  authors={AUTHOR.join(", ")}
+			  title={TITLE}
+			  url={URL}
+			  journal={JOURNAL}
+			  year={YEAR.toString()}
+			  month={MONTH}
+			  volume={VOLUME}
+			  issue={NUMBER}
+			  pages={PAGES}
+			/>
+		    </div>
+		  );
+		})
+	    }
+	    </FlexStyled>
 	</section>
     </Layout>
   );
