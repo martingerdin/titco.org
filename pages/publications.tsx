a6981f07ff49b5b9300a89a3384a0b9e2ac8aa40
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-05-06 00:20:04 +0200

    Improve filtering by year

--- pages/publications.tsx
+++ pages/publications.tsx
@@ -5,64 +5,73 @@ import Layout from "../components/Layout";
 import { useState } from "react";
 
 const FlexStyled = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
 `;
 
 export default function PublicationsPage() {
-  const [currentYear, setCurrentYear] = useState("all");
   const sortedPublications = [...publications]
     .sort((a, b) => {
       return b.YEAR - a.YEAR;
     });
   const years = [...new Set(sortedPublications.map((publication) => publication.YEAR))];
+  const [selectedYears, setSelectedYears] = useState(years);
   return (
     <Layout title="Publications" subtitle={`Our published work ${years[years.length - 1]}-${new Date().getUTCFullYear()}`}>
 	<div className="buttons is-centered has-addons">
 	    <button
-	      className={`button ${currentYear === "all" ? "is-selected is-primary" : ""}`}
-	      onClick={() => setCurrentYear("all") }
+	      className={
+	      `button ${years.every(year => selectedYears.includes(year)) ? "is-selected is-primary" : ""}`
+	      }
+	      onClick={() => {
+		setSelectedYears(years)
+	      }}
 	    >
 		All years
 	    </button>
 	    {years.sort().map((year, key) => {
 	      return <button
 		       key={key}
-		       className={`button ${currentYear === year.toString() ? "is-selected is-primary" : ""}`}
-		       onClick={() => setCurrentYear(year.toString()) }>{year}</button>
+		       className={`button ${selectedYears.includes(year) ? "is-selected is-primary" : ""}`}
+		       onClick={() => {
+			 if (years.every(year => selectedYears.includes(year))) {
+			   setSelectedYears([year]);
+			 } else if (!selectedYears.includes(year)) {
+			   setSelectedYears([...selectedYears, year]);
+			 } else if (selectedYears.includes(year)) {
+			   setSelectedYears(selectedYears.filter(selectedYear =>  selectedYear !== year));
+			 }
+		       }
+		       }>
+		  {year}
+	      </button>
 	    })}
 	</div>
 	<section className="section">
 	    <FlexStyled>
-	    {
-	      sortedPublications
-		.filter(publication => {
-		  if (currentYear === "all") {
-		    return true;
-		  } else {
-		    return publication.YEAR.toString() === currentYear;
-		  }
-		})
-		.map((publication, key) => {
-		  const {AUTHOR, TITLE, VOLUME, NUMBER, URL, JOURNAL, YEAR, MONTH, PAGES} = publication;
-		  return (
-		    <div className="mx-4 block" key={key}>
-			<Publication
-			  authors={AUTHOR.join(", ")}
-			  title={TITLE}
-			  url={URL}
-			  journal={JOURNAL}
-			  year={YEAR.toString()}
-			  month={MONTH}
-			  volume={VOLUME}
-			  issue={NUMBER}
-			  pages={PAGES}
-			/>
-		    </div>
-		  );
-		})
-	    }
+		{
+		  sortedPublications
+		    .filter(publication => selectedYears.includes(publication.YEAR))
+		    .map((publication, key) => {
+		      const {AUTHOR, TITLE, VOLUME, NUMBER, URL, JOURNAL, YEAR, MONTH, PAGES} = publication;
+		      return (
+			<div className="mx-4 block" key={key}>
+			    <Publication
+			      authors={AUTHOR.join(", ")}
+			      title={TITLE}
+			      url={URL}
+			      journal={JOURNAL}
+			      year={YEAR.toString()}
+			      month={MONTH}
+			      volume={VOLUME}
+			      issue={NUMBER}
+			      pages={PAGES}
+			    />
+			</div>
+		      );
+		    })
+		}
 	    </FlexStyled>
 	</section>
     </Layout>
