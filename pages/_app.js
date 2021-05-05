83b3792d565eaad1181dead71dba566de565550e
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-05-06 00:27:54 +0200

    Add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import "../styles/styles.scss";
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
