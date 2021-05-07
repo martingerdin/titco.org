1b989e0ccc3aca532a437fd84a02e24013d9be7b
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-05-07 10:59:44 +0200

    Refactor Publication component and add copy citation functionality

--- components/Publication.tsx
+++ components/Publication.tsx
@@ -2,7 +2,9 @@ import Icon from "@mdi/react";
 import { mdiOpenInNew } from "@mdi/js";
 import Link from "next/link";
 import styled from "styled-components";
+import copy from "copy-to-clipboard";
 import capitalise from "../lib/capitalise";
+import { useState } from "react";
 
 const CardStyled = styled.div`
     max-width: 400px;
@@ -21,24 +23,67 @@ interface publicationProps {
 }
 
 export default function Publication({ authors, title, url, journal, year, month, volume, issue, pages }: publicationProps) {
+  const [citationButtonText, setCitationButtonText] = useState("Citation");
   if (typeof month !== "undefined") { 
     var Month = capitalise(month);
   } else {
     var Month = "";
   }
-  
+  if (typeof issue === "undefined") {
+    issue = "";
+  }
   return (
     <>
 	<CardStyled className="card">
+	    <header className="card-header">
+		<div className="card-header-title">
+		    <div className="tags">
+			<span className="tag is-white">{journal !== "" ? journal : ""}</span>
+			<span className="tag is-primary is-light">{year}</span>
+		    </div>
+		</div>
+	    </header>
 	    <div className="card-content">
 		<p className="title is-4 is-spaced">{title}</p>
 		<p className="subtitle is-6">{authors}</p>
-		<p className="content"><i>{journal}. {year} {Month};{volume}({issue}):{pages}.</i></p>
 	    </div>
-	    
-	    {typeof url !== "undefined" &&
-	     <footer className="card-footer">
-
+	    <footer className="card-footer">
+		<div className="card-footer-item dropdown is-hoverable"
+		     onMouseOver={() => setCitationButtonText("Copy Citation")}
+		     onMouseOut={() => setCitationButtonText("Citation")}
+		>
+		    <a className="dropdown-trigger"
+		       onClick={() => {
+			 const copyWorked = copy(
+			   authors + ". " + title + ". " + journal + ". " + year + "  " + Month + ";" + volume + "(" + issue + ")" + ":" + pages + "."
+			 );
+			 if (copyWorked) {
+			   setCitationButtonText("Citation Copied!");
+			 }
+		       }
+		       }>
+			{citationButtonText}
+		    </a>
+		    <div className="dropdown-menu" id="dropdown" role="menu">
+			<div className="dropdown-content">
+			    <div className="dropdown-item">
+				<p className="content">
+				    {authors !== "" ? authors + ". " : ""}
+				    {title !== "" ? <b>{title}. </b> : ""}
+				    <i>
+					{journal !== "" ? journal + ". " : ""}
+					{year !== "" ? year + " " : ""}
+					{Month !== "" ? Month + ";" : ""}
+					{volume !== "" ? volume : ""}
+					{issue !== "" ? "("+issue+"):" : ""}
+					{pages !== "" ? pages : ""}.
+				    </i>
+				</p>
+			    </div>
+			</div>
+		    </div>
+		</div>
+		{typeof url !== "undefined" &&
 		 <Link href={url} passHref>
 		     <a className="card-footer-item">
 			 <span>Fulltext</span>
@@ -48,8 +93,9 @@ export default function Publication({ authors, title, url, journal, year, month,
 		     </a>
 		 </Link>
 
-	     </footer>
-	    }
+
+		}
+	    </footer>
         </CardStyled>
     </>
   )  
