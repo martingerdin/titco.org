937de81a250a22a07bd6d2b8645f0a3fdb5d350b
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-06-08 14:01:10 +0200

    Make map data dynamic

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,60 +1,53 @@
-import { Chart } from "react-google-charts";
 import { GoogleCharts } from "google-charts";
 import { useEffect } from "react";
 
 interface mapProps {
-  apiKey: string;
+  data: location[];
 }
 
-export function Map () {
+interface location {
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
+export function Map({ data }: mapProps) {
   useEffect(() => {
-    const drawMap = () => {
-      const data = GoogleCharts.api.visualization.arrayToDataTable([
-	[
-	  {type: "number", id: "Latitude"},
-	  {type: "number", id: "Longitude"},
-	  {type: "string", id: "Name"}
-	],
-	[
-	  19.036941675407327,
-	  72.86009054046254,
-	  "LTMGH"
-	],
-	[
-	  19.002646061161908,
-	  72.84205544046206,
-	  "KEM"
-	],
-	[
-	  28.56830255340446,
-	  77.20077372896768,
-	  "JPNATC"
-	],
-	[
-	  22.539791106134892,
-	  88.34254487862368,
-	  "SSKM"
-	],
-      ]);
+    const head = [
+      { type: "number", id: "Latitude" },
+      { type: "number", id: "Longitude" },
+      { type: "string", id: "Name" },
+    ];
+
+    function drawMap() {
+      const mapData = GoogleCharts.api.visualization.arrayToDataTable(
+        [].concat(
+          [head],
+          data.map((location) => {
+            const { latitude, longitude, name } = location;
+            return [latitude, longitude, name];
+          })
+        )
+      );
 
       var options = {
-	showTooltip: true,
-	showInfoWindow: false,
-	mapType: "terrain"
+        showTooltip: true,
+        showInfoWindow: false,
+        mapType: "terrain",
       };
 
-      var map = new GoogleCharts.api.visualization.Map(document.getElementById('chart_div'));
+      var map = new GoogleCharts.api.visualization.Map(
+        document.getElementById("chart_div")
+      );
 
-      map.draw(data, options);
-    };
+      map.draw(mapData, options);
+    }
 
     GoogleCharts.load(drawMap, {
-      "packages": ["map"],
-      "mapsApiKey": process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+      packages: ["map"],
+      mapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
     });
-  })
+  });
 
-  return (
-    <div id="chart_div" style={{width: "100%", height: "250px"}}></div>
-  );
+  return <div id="chart_div" style={{ width: "100%", height: "100%" }}></div>;
 }
