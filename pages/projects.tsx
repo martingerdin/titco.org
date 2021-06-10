c2598df1f27763118939a3508a47a235036dd48b
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-06-10 14:03:48 +0200

    Draft projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,44 @@
+import { join } from "path";
+import fs from "fs";
+import matter from "gray-matter";
 import Layout from "../components/Layout";
 
-export default function ProjectsPage() {
+export default function ProjectsPage({ projects }: any) {
   return (
     <Layout title="Projects">
-	<p>Hello</p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {projects.map((project: any, key: number) => {
+          const projectMatter = matter(project);
+          const { title, subtitle } = projectMatter.data;
+          return (
+            <div className="block mx-4" key={key}>
+              <article className="card" style={{ maxWidth: "400px" }}>
+                <div className="card-content">
+                  <p className="title is-4">{title}</p>
+                  <p className="subtitle is-6">{subtitle}</p>
+                </div>
+              </article>
+            </div>
+          );
+        })}
+      </div>
     </Layout>
   );
+}
+
+const projectDir = join(process.cwd(), "_projects");
+
+export async function getStaticProps(context: any) {
+  const { params } = context;
+  const projects = fs
+    .readdirSync(projectDir)
+    .map((file) => fs.readFileSync(join(projectDir, file), "utf8"));
+  return {
+    props: { projects },
+  };
 }
