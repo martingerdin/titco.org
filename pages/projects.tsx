caff9c44a0813d31887c4447331805bfb7f82dc9
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-08-06 13:47:10 +0200

    Fix project links

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,13 +8,15 @@ import { TagList } from "../components/TagList";
 
 interface projectsPageProps {
   projectFiles: string[];
-  projectsData: string[];
+  projectsData: Project[];
 }
 
-export default function ProjectsPage({
-  projectFiles,
-  projectsData,
-}: projectsPageProps) {
+interface Project {
+  file: string;
+  content: any;
+}
+
+export default function ProjectsPage({ projectsData }: projectsPageProps) {
   return (
     <Layout title="Projects" subtitle="Our Ongoing and Completed Projects">
       <section className="section">
@@ -25,72 +27,77 @@ export default function ProjectsPage({
             justifyContent: "center",
           }}
         >
-            {projectsData
-	      .map((project: any) => {return(matter(project).data)})
-	      .sort((a, b) => b.start - a.start)
-	      .map((project: any, key: number) => {
-            const {
-              title,
-              subtitle,
-              aim,
-              status,
-              start,
-              end,
-              centres,
-              cities,
-              sampleSize,
-              targetSampleSize,
-            } = project;
-            let sampleSizeKey;
-            let sampleSizeValue;
-            if (typeof targetSampleSize !== "undefined") {
-              sampleSizeKey = "Target Sample Size";
-              sampleSizeValue = targetSampleSize;
-            } else if (typeof sampleSize !== "undefined") {
-              sampleSizeKey = "Sample Size";
-              sampleSizeValue = sampleSize;
-            }
-            const tags = [
-              { heading: "Status", value: status },
-              { heading: "Start", value: start },
-            ];
-            if (typeof end !== "undefined")
-              tags.push({ heading: "End", value: end });
-            const projectPage = projectFiles[key].replace(".md", "");
-            return (
-              <div className="block mx-4" key={key}>
-                <article
-                  className="card"
-                  style={{ maxWidth: "600px", minWidth: "300px" }}
-                >
-                  <div className="card-content">
-                    <div className="content">
-                      {typeof tags !== "undefined" && <TagList tags={tags} />}
+          {projectsData
+            .map((project: Project) => {
+              return {
+                file: project.file,
+                content: matter(project.content).data,
+              };
+            })
+            .sort((a, b) => b.content.start - a.content.start)
+            .map((project: Project, key: number) => {
+              const {
+                title,
+                subtitle,
+                aim,
+                status,
+                start,
+                end,
+                centres,
+                cities,
+                sampleSize,
+                targetSampleSize,
+              } = project.content;
+              let sampleSizeKey;
+              let sampleSizeValue;
+              if (typeof targetSampleSize !== "undefined") {
+                sampleSizeKey = "Target Sample Size";
+                sampleSizeValue = targetSampleSize;
+              } else if (typeof sampleSize !== "undefined") {
+                sampleSizeKey = "Sample Size";
+                sampleSizeValue = sampleSize;
+              }
+              const tags = [
+                { heading: "Status", value: status },
+                { heading: "Start", value: start },
+              ];
+              if (typeof end !== "undefined")
+                tags.push({ heading: "End", value: end });
+              const projectPage = project.file.replace(".md", "");
+              return (
+                <div className="block mx-4" key={key}>
+                  <article
+                    className="card"
+                    style={{ maxWidth: "600px", minWidth: "300px" }}
+                  >
+                    <div className="card-content">
+                      <div className="content">
+                        {typeof tags !== "undefined" && <TagList tags={tags} />}
+                      </div>
+                      <p className="title is-4">{title}</p>
+                      <p className="subtitle is-6">{subtitle}</p>
+                      <hr />
+                      <ProjectLevel
+                        isSmall={true}
+                        items={[
+                          { Centres: Object.keys(centres).length },
+                          { Cities: cities },
+                          { [sampleSizeKey]: sampleSizeValue },
+                        ]}
+                      />
+                      <hr />
+                      <div className="content">
+                        <p className="title is-5">Aim</p>
+                        <p>{aim}</p>
+                      </div>
+                      <Link href={`/projects/${projectPage}`}>
+                        <a className="button">Read More</a>
+                      </Link>
                     </div>
-                    <p className="title is-4">{title}</p>
-                    <p className="subtitle is-6">{subtitle}</p>
-                    <hr />
-                    <ProjectLevel
-                      isSmall={true}
-                      items={[
-                        { Centres: Object.keys(centres).length },
-                        { Cities: cities },
-                        { [sampleSizeKey]: sampleSizeValue },
-                      ]}
-                    />
-                    <hr />
-                    <div className="content">
-                      <p className="title is-5">Aim</p>
-                      <p>{aim}</p>
-                    </div>
-                    <Link href={`/projects/${projectPage}`}>
-                      <a className="button">Read More</a>
-                    </Link>
-                  </div>
-                </article>
-              </div>
-            );
-          })}
+                  </article>
+                </div>
+              );
+            })}
         </div>
       </section>
     </Layout>
@@ -101,10 +108,13 @@ const projectDir = join(process.cwd(), "_projects");
 
 export async function getStaticProps() {
   const projectFiles = fs.readdirSync(projectDir);
-  const projectsData = projectFiles.map((file) =>
-    fs.readFileSync(join(projectDir, file), "utf8")
-  );
+  const projectsData = projectFiles.map((file) => {
+    return {
+      file: file,
+      content: fs.readFileSync(join(projectDir, file), "utf8"),
+    };
+  });
   return {
-    props: { projectFiles, projectsData },
+    props: { projectsData },
   };
 }
