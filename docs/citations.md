59bfef9f70e99d6cb1355718d83d58dc35335784
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-08-26 15:03:29 +0200

    Add publications as bibtex and start writing a parser

---
+++ docs/citations.md
@@ -0,0 +1,3 @@
+# Publications
+
+To add a publication, please find the doi of the publication that you want to add. The doi is easy to find from for example PubMed, where it is located just below the authors list. Once you have the doi please use www.doi2bib.org to get a BibTeX entry. 
--- pages/publications.tsx
+++ pages/publications.tsx
@@ -1,82 +1,175 @@
+import { join } from "path";
+import fs from "fs";
 import publications from "../components/publications.json";
 import Publication from "../components/Publication";
 import Layout from "../components/Layout";
 import { useState } from "react";
 
 export default function PublicationsPage() {
-  const sortedPublications = [...publications]
-    .sort((a, b) => {
-      return b.YEAR - a.YEAR;
-    });
-  const years = [...new Set(sortedPublications.map((publication) => publication.YEAR))];
+  const sortedPublications = [...publications].sort((a, b) => {
+    return b.YEAR - a.YEAR;
+  });
+  const years = [
+    ...new Set(sortedPublications.map((publication) => publication.YEAR)),
+  ];
   const [selectedYears, setSelectedYears] = useState(years);
   // Make sure that all publications are shown if the user deselects all years
   if (selectedYears.length === 0) {
     setSelectedYears(years);
   }
-  const selectedPublications =  sortedPublications
-    .filter(publication => selectedYears.includes(publication.YEAR))
+  const selectedPublications = sortedPublications.filter((publication) =>
+    selectedYears.includes(publication.YEAR)
+  );
   return (
-    <Layout title="Publications" subtitle={`Our published work ${years[years.length - 1]}-${new Date().getUTCFullYear()}`}>
-	<div className="buttons is-centered has-addons">
-	    <button
-	      className={
-	      `button ${years.every(year => selectedYears.includes(year)) ? "is-selected is-primary" : ""}`
-	      }
-	      onClick={() => {
-		setSelectedYears(years)
-	      }}
-	    >
-		All years
-	    </button>
-	    {years.sort().map((year, key) => {
-	      return <button
-		       key={key}
-		       className={`button is-outlined ${selectedYears.includes(year) ? "is-selected is-primary" : ""}`}
-		       onClick={() => {
-			 if (years.every(year => selectedYears.includes(year))) {
-			   setSelectedYears([year]);
-			 } else if (!selectedYears.includes(year)) {
-			   setSelectedYears([...selectedYears, year]);
-			 } else if (selectedYears.includes(year)) {
-			   setSelectedYears(selectedYears.filter(selectedYear =>  selectedYear !== year));
-			 } 
-		       }
-		       }
-		     >
-		  {year}
-	      </button>
-	    })}
-	</div>
-	<div className="content has-text-centered">
-	    <p className="subtitle">Showing {selectedPublications.length} of {publications.length} publications.</p>
-	</div>
-	<section className="section">
-	    <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
-		{
-		  // Show only those publications published in years selected by the user
-		  selectedPublications
-		    .map((publication, key) => {
-		      const {AUTHOR, TITLE, VOLUME, NUMBER, URL, JOURNAL, YEAR, MONTH, PAGES} = publication;
-		      return (
-			<div className="mx-4 block" key={key}>
-			    <Publication
-			      authors={AUTHOR.join(", ")}
-			      title={TITLE}
-			      url={URL}
-			      journal={JOURNAL}
-			      year={YEAR.toString()}
-			      month={MONTH}
-			      volume={VOLUME}
-			      issue={NUMBER}
-			      pages={PAGES}
-			    />
-			</div>
-		      );
-		    })
-		}
-	    </div>
-	</section>
+    <Layout
+      title="Publications"
+      subtitle={`Our published work ${
+        years[years.length - 1]
+      }-${new Date().getUTCFullYear()}`}
+    >
+      <div className="buttons is-centered has-addons">
+        <button
+          className={`button ${
+            years.every((year) => selectedYears.includes(year))
+              ? "is-selected is-primary"
+              : ""
+          }`}
+          onClick={() => {
+            setSelectedYears(years);
+          }}
+        >
+          All years
+        </button>
+        {years.sort().map((year, key) => {
+          return (
+            <button
+              key={key}
+              className={`button is-outlined ${
+                selectedYears.includes(year) ? "is-selected is-primary" : ""
+              }`}
+              onClick={() => {
+                if (years.every((year) => selectedYears.includes(year))) {
+                  setSelectedYears([year]);
+                } else if (!selectedYears.includes(year)) {
+                  setSelectedYears([...selectedYears, year]);
+                } else if (selectedYears.includes(year)) {
+                  setSelectedYears(
+                    selectedYears.filter(
+                      (selectedYear) => selectedYear !== year
+                    )
+                  );
+                }
+              }}
+            >
+              {year}
+            </button>
+          );
+        })}
+      </div>
+      <div className="content has-text-centered">
+        <p className="subtitle">
+          Showing {selectedPublications.length} of {publications.length}{" "}
+          publications.
+        </p>
+      </div>
+      <section className="section">
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {
+            // Show only those publications published in years selected by the user
+            selectedPublications.map((publication, key) => {
+              const {
+                AUTHOR,
+                TITLE,
+                VOLUME,
+                NUMBER,
+                URL,
+                JOURNAL,
+                YEAR,
+                MONTH,
+                PAGES,
+              } = publication;
+              return (
+                <div className="mx-4 block" key={key}>
+                  <Publication
+                    authors={AUTHOR.join(", ")}
+                    title={TITLE}
+                    url={URL}
+                    journal={JOURNAL}
+                    year={YEAR.toString()}
+                    month={MONTH}
+                    volume={VOLUME}
+                    issue={NUMBER}
+                    pages={PAGES}
+                  />
+                </div>
+              );
+            })
+          }
+        </div>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const publicationsFile = fs.readFileSync(
+    join(process.cwd(), "_publications/publications.bib"),
+    "utf8"
+  );
+  const publicationsData = publicationsFile
+    .split("@")
+    .map((entry: string) => {
+      return (
+        entry
+          .split("\n")
+          .map((line) => {
+            return line
+              .replace(/^article\{([\w]*)\,/, "key = $1,")
+              .replace(/^\s*([a-z]*)\s=\s{?/, '"$1": "')
+              .replace(/\}?\,?\s*$/, '",')
+              .replace(/^"\,$/, "");
+          })
+          .join("\n")
+          // Replace strange letters
+          .replace(/{\\"a}/g, "&x00E4")
+          .replace(/{\\aa}/g, "&x00E5")
+          .replace(/{\\"o}/g, "&x00F6")
+          .replace(/{\\"A}/g, "&x00C4")
+          .replace(/{\\AA}/g, "&x00C5")
+          .replace(/{\\"O}/g, "&x00C6")
+          // Convert bibtex to something that can be parsed as json
+          .replace(/\{/g, "")
+          .replace(/\}/g, "")
+      );
+    })
+    .filter((entry) => entry !== "")
+    .map((entry) => {
+      let jsonEntry = entry;
+      const i = entry.lastIndexOf(",");
+      if (i != -1) {
+        jsonEntry = jsonEntry.substr(0, i);
+      }
+      const json = "{" + jsonEntry + "}";
+      console.log(json);
+      let jsonParsed;
+      try {
+        jsonParsed = JSON.parse(json);
+      } catch (error) {
+        jsonParsed = {
+          message: "The publication could not be parsed",
+          error: "",
+        };
+      }
+      return jsonParsed;
+    });
+  console.log(publicationsData);
+  return {
+    props: { publicationsFile },
+  };
 }
