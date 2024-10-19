6c6c0696ae2d66092fff513b758aedcb03990ee8
martingerdin <martingerdin@gmail.com>
2024-10-19 18:05:37 +0530

    Trying to fix the map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -14,7 +14,7 @@ interface location {
 export function Map({ data }: mapProps) {
   const [mapIsLoaded, setMapIsLoaded] = useState(false);
   const googlemap = useRef(null);
-  
+
   useEffect(() => {
     const head = [
       { type: "number", id: "Latitude" },
@@ -24,29 +24,34 @@ export function Map({ data }: mapProps) {
 
     function drawMap() {
       const mapData = GoogleCharts.api.visualization.arrayToDataTable(
-	[].concat(
+        [].concat(
           [head],
           data.map((location) => {
             const { latitude, longitude, name } = location;
             return [latitude, longitude, name];
           })
-	)
+        )
       );
 
       var options = {
-	showTooltip: true,
-	showInfoWindow: false,
-	mapType: "terrain",
+        showTooltip: true,
+        showInfoWindow: false,
+        mapType: "terrain",
       };
 
       var map = new GoogleCharts.api.visualization.Map(
-	googlemap.current,
+        googlemap.current,
       );
 
-      setMapIsLoaded(true);
-      map.draw(mapData, options);
+      try {
+        setMapIsLoaded(true);
+        map.draw(mapData, options);
+      } catch (error) {
+        console.error("Error drawing map:", error);
+        setMapIsLoaded(false);
+      }
     }
-    
+
     GoogleCharts.load(drawMap, {
       packages: ["map"],
       mapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
@@ -58,26 +63,30 @@ export function Map({ data }: mapProps) {
 
   return (
     <div className="has-ratio">
-	<div
-	  className="has-background-light"
-	  style={{
-            display: `${!mapIsLoaded ? "flex" : "none"}`,
-	    alignItems: "center",
-	    justifyContent: "center",
-            width: "100%",
-            height: "100%",
-	  }}>
-	    <span className="button is-loading is-light"></span>
-	</div>
-	<div
-	  id="chart_div"
-	  ref={googlemap}
-	  style={{
-            display: `${mapIsLoaded ? "block" : "none"}`,
-	    width: "100%",
-            height: "100%",
-	  }}
-	></div>
+      <div
+        className="has-background-light"
+        style={{
+          display: `${!mapIsLoaded ? "flex" : "none"}`,
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+        }}>
+        {!mapIsLoaded ? (
+          <span className="button is-loading is-light"></span>
+        ) : (
+          <span>Error loading map</span>
+        )}
+      </div>
+      <div
+        id="chart_div"
+        ref={googlemap}
+        style={{
+          display: `${mapIsLoaded ? "block" : "none"}`,
+          width: "100%",
+          height: "100%",
+        }}
+      ></div>
     </div>
   );
 }
